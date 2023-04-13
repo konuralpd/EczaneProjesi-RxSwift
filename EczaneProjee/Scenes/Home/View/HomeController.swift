d53fe0564fd6f50c68a4f7211fc35828ebc2091c
Ekrem Alkan <ae.alkanekrem@gmail.com>
2023-04-13 15:12:22 +0300

    setup
    setup

---
+++ EczaneProjee/Scenes/Home/View/HomeController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeController.swift
+//  EczaneProjee
+//
+//  Created by Ekrem Alkan on 13.04.2023.
+//
+
+import UIKit
+
+final class HomeController: UIViewController {
+
+    //MARK: - Life Cycle Methods
+    
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .yellow
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+  
+
+}
